dee979bf4b924981ebb68c561485bf1b6f1afa4a
KichwaMbaya <kiptoosiele@yahoo.com>
2023-09-03 11:53:22 -0700

    My first commit of UK android App

---
+++ app/src/main/java/com/justkeepfaith/umorototena/Notification_Alarm.java
@@ -0,0 +1,39 @@
+package com.justkeepfaith.umorototena;
+
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class Notification_Alarm extends BroadcastReceiver {
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Intent intent1 = new Intent(context.getApplicationContext(), striscia_pagina.class);
+        intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent activity = PendingIntent.getActivity(context.getApplicationContext(),
+                0, intent1, PendingIntent.FLAG_IMMUTABLE);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "Successful")
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Congratulations!! Your loan application has been successful.")
+                .setContentText("Your loan application has been approved. Please log into the app for disbursement.")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Your loan application has been approved. Please log into the app for disbursement."))
+                .setAutoCancel(true)
+                .setContentIntent(activity);
+
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        notificationManager.notify(200, builder.build());
+    }
+}
